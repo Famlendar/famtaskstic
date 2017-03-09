e8fbd5b143cb278f5b74e63a6f49279d50a5328c
Will E. Chapman <willechapman@gmail.com>
2017-03-09 00:41:14 -0800

    In main.js I created checkTime to add 0 to sindle digits and startTime function to store currentTime, chane to show standard time, and organizedisplay order with an EventListener to display date  and time. In main.css I changed backgroud and font color/style.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
-var today = new Date();
-var el = document.getElementById('js-dateTime');
-setTimeout(function domListenerEvent() {
-	el.textContent = today.toDateString();
-}, 5000);
-el.addEventListner('onload', domListenerEvent(), false);
+// Date and Time Display
+// create var for section element
+var dateTime = document.getElementById('js-dateTime');
+// Append Date Time class to Date Time section element by id
+dateTime.className += 'dateTime';
 
+// function to add a zero in front of the number if the number is less than 10
+function checkTime(i) {
+    // check if number is less than 10
+    if (i < 10) {
+        // adds zero in front of number
+        i = "0" + i
+    }
+
+    // send back new value
+    return i
+}
+
+// runs on page load 
+function startTime() {
+    // store the time in a variable
+    var currentTime = new Date()
+
+    // store the current hours, minutes and seconds in variables
+    var month = currentTime.getMonth()
+    var day = currentTime.getDate()
+    var year = currentTime.getFullYear()
+    var hour = currentTime.getHours()
+    var minute = currentTime.getMinutes()
+    var second = currentTime.getSeconds()
+ 
+    //change military time to standard time if hours is greater than 12
+    if (hour > 12) {
+        // remove 12 from hour
+        hour = hour - 12;
+    }
+
+    // update variables minute and second using checkTime function from line 1
+    hour = checkTime(hour)
+    minute = checkTime(minute)
+    second = checkTime(second)
+
+    //select element with id="js-dateTime" and change the text to the current time
+    dateTime.textContent = month + '/' + day + '/' + year + '  ' + hour + ":" + minute + ":" + second;
+
+    //recalls the function every second, time becomes dynamic
+    setTimeout(function () {
+        startTime();
+    }, 1000)
+}
+
+// add event listener to start function so finally time will be displayed
+dateTime.addEventListener('onload', startTime(), false);
